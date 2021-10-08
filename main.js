da27fc3be971b29cf0d178c736cc785aa7e1ccbc
Héctor Ulloa <ulloazhector@gmail.com>
2021-10-08 19:20:51 -0300

    Desafio 3

--- main.js
+++ main.js
@@ -1,21 +1,5 @@
-var name = prompt("Ingrese su nombre");
-var hour = parseInt( prompt("Ingrese la hora (00 - 23)") );
-var validTime = (hour >= 0 && hour < 24);
+//Cuadrado de los primeros n números
+var n = parseInt(prompt("Ingresar número"));
 
-
-if (name != "") {
-
-    if (validTime) {
-        // Datos válidos
-        if (hour < 7 || hour > 19)
-            alert("¡Buenas noches " + name + '!');
-        else if (hour < 13)
-            alert("¡Buenos días "   + name + '!');
-        else
-            alert("¡Buenas tardes " + name + '!');
-    }
-    else
-        alert("Hora invalida");
-}
-else
-    alert("Nombre no ingresado");
+for (let i = 1; i <= n; i++)
+    console.log(`${i}^2 = ${i**2}`);
