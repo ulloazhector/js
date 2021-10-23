31f8b1c493fcc19840b333f1ab56ce802a619092
Héctor Ulloa <ulloazhector@gmail.com>
2021-10-23 00:26:47 -0300

    Desafio 6

--- main.js
+++ main.js
@@ -1,103 +1,114 @@
 
-// Valores de CFT
-// const CFT3  = .35;
-// const CFT6  = .48;
-// const CFT9  = .87;
-// const CFT12 = 1.13;
-// const CFT18 = 2.01;
+// Declaración de clases
+// Transferencia
+class Transferencia {
 
-
-
-// // Definicion de funciones
-// function solicitarPrestamo(){
-//     return parseInt(prompt("Ingresar valor del prestamo"));
-// }
-
-// function calculoCuotas(){
-//     switch (cuotasCant) {
-//         case 3: return (prestamo + prestamo*CFT3)/3;
-//         case 6: return (prestamo + prestamo*CFT6)/6;
-//         case 9: return (prestamo + prestamo*CFT9)/9;
-//         case 12: return (prestamo + prestamo*CFT12)/12;
-//         default: return (prestamo + prestamo*CFT18)/18;
-//     }
-// }
-
-// function mostrar(valor, cant){
-//     alert("En total vas a pagar " + cant + " cuotas de $" + valor.toFixed(3) + " cada una.\nTOTAL a pagar $" + cant*valor);
-// }
-     
-
-
-// // Main
-// let cantOk;
-// let prestamo = solicitarPrestamo();
-// do{
-//     cuotasCant = parseInt(prompt("Cantidad de cuotas: 3 / 6 / 9 / 12 / 18"));
-//     cantOk = (cuotasCant == 3) || (cuotasCant == 6) ||
-//              (cuotasCant == 9) || (cuotasCant == 12) ||
-//              (cuotasCant == 18);
-// }while(!cantOk);
-
-// let valorCuota = calculoCuotas(cuotasCant);
-// mostrar(valorCuota, cuotasCant);
-
-
-
-// Objeto
-class Wallet{
-
-    constructor(user){
-        this.usuario = user;
-        this.saldo = 0;
+    constructor(rec, amount, fecha) {
+        this.rec    = rec;          // true: dinero recibido, false: dinero enviado
+        this.amount = amount;
+        this.fecha  = fecha         // En formato de entero
     }
 
-    ingresar(valor){
-        this.saldo += valor;
-        alert(`Ingresaste $${valor} a tu cuenta`);
-    }
-
-    transferir(cuenta, valor){
-
-        if(this.saldo >= valor){
-            this.saldo -= valor;
-            alert(`Transferiste $${valor} a la cuenta ${cuenta}`);
-        } 
-        else
-            alert(`Saldo insuficiente`);
-    }
-
-    consultarInfo(){
-        alert(`Usuario: ${this.usuario}\nSaldo actual: $${this.saldo}`);
+    info(){
+        const sign = this.rec ? "+" : "-";
+        console.log(`${sign} $${this.amount}\n  Fecha: ${this.fecha}`);
     }
 }
 
 
 
 
+// Billetera
+class Wallet{
+    constructor(user){
+        this.usuario     = user;
+        this.saldo       = 0;
+        this.n           = 0;       // Cantidad de transferencias
+        this.arrayTransf = [];      // Array de objetos transferencias
+    }
+    
+    ingresar(valor, fecha){
+        if(valor >= 0){
+            this.saldo += valor;
+            this.arrayTransf.push( new Transferencia(true, valor, fecha) );
+            this.n++;
+
+            alert(`Ingresaste $${valor} a tu cuenta.`);
+        } else
+        alert(`Valor incorrecto!`);
+    }
+    
+    pagar(valor, fecha){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+            this.arrayTransf.push( new Transferencia(false, valor, fecha) );
+            this.n++;
+
+            alert(`Pagaste $${valor}.`);
+        } 
+        else
+        alert(`Saldo insuficiente.`);
+    }
+    
+    consultarInfo(){
+        console.log(`\n\n${this.n} transferencias\n`);
+        
+        for (let i = 0; i < this.n; i++)
+            this.arrayTransf[i].info();
+
+        alert(`Usuario: ${this.usuario}\nSaldo actual: $${this.saldo}.`);
+    }
+
+    sortedInfo(ordenarPor){
+        let backup = this.arrayTransf;
+
+        this.arrayTransf.sort((a,b) => a[ordenarPor] - b[ordenarPor]);
+        
+        this.consultarInfo();
+        this.arrayTransf = backup;
+    }
+}
+// Fin Declaración de clases
+
+
+
+
+//----------MAIN----------
+
 // Creo una sola billetera
 const myWallet = new Wallet(prompt("Ingresar usuario de la billetera"));
-let alias, amount, option;
+let amount, fecha, option;
+
 
 
 // Menú principal
 do {
-    option = parseInt(prompt("OPCIONES\n(1) Ingresar dinero\n(2) Transferir dinero\n(3) Consultar información"));
+    option = parseInt(prompt("Opciones\n\n" +
+                            "(1) Ingresar dinero\n" +
+                            "(2) Pagar\n" +
+                            "(3) Ver movimientos"));
 
     switch (option) {
         case 1:
             amount = parseFloat(prompt("Monto a ingresar"));
-            myWallet.ingresar(amount);
+            fecha = parseInt(prompt("Fecha:"));
+            myWallet.ingresar(amount, fecha);
             break;
 
         case 2:
-            alias = prompt("Alias de la cuenta destino");
-            amount = parseFloat(prompt("Monto a transferir"));
-            myWallet.transferir(alias, amount);
+            amount = parseFloat(prompt("Monto a pagar"));
+            fecha = parseInt(prompt("Fecha:"));
+            myWallet.pagar(amount, fecha);
             break;
 
         case 3:
-            myWallet.consultarInfo();
+            let option2 = parseInt(prompt(`Ordenar por:\n\n(1) Sin ordenar\n(2) Fecha\n(3) Monto`));
+            switch(option2){
+                case 1: myWallet.consultarInfo(); break;
+                case 2: myWallet.sortedInfo("fecha"); break;
+                case 3: myWallet.sortedInfo("amount"); break;
+                default:;
+            }
             break;
 
         default:
