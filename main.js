e825a71d7db5ec0e60e6c47e2d9adb23937ad534
Héctor Ulloa <ulloazhector@gmail.com>
2021-11-04 21:33:05 -0300

    Desafio 9 y complementario

--- main.js
+++ main.js
@@ -1,132 +1,104 @@
 
-// Declaración de clases
-// Transferencia
-class Transferencia {
+//              Clase Tarjeta
+class Tarjeta{
+    constructor(name, tarjeta, number, ccv){
+        this.name = name;
+        this.tarjeta = tarjeta;
+        this.number = number;
+        this.ccv = ccv;
 
-    constructor(rec, amount, fecha) {
-        this.rec    = rec;          // true: dinero recibido, false: dinero enviado
-        this.amount = amount;
-        this.fecha  = fecha         // En formato de entero
     }
+}
 
-    info(){
-                                    // Hago referencia a la lista creada en consultarInfo()
-        let nodoLista = document.getElementById("listaTransferencias");
-        let li = document.createElement("li");
+//              Nodos principales
+let addCards = document.getElementById(`addCards`);
+let listaDeTarjetas = document.getElementById(`listaDeTarjetas`);
+let formNewCard = document.getElementById(`formNewCard`);
+
+addCards.addEventListener(`click`, addCreditCardForm);
+
+
+
+
+//              Event handlers
+
+function addCreditCardForm(){
+    // Creo el formulario
+    formNewCard.innerHTML = `
+        <input placeholder="Títular" id="nombre"><br>
+        <input placeholder="Tarjeta" id="tarjeta"><br>
+        <input placeholder="Número" id="numero"><br>
+        <input placeholder="CCV" id="ccv"><br>
+    `;
+
+    // Agrego el botón
+    let boton = document.createElement(`button`);
+    boton.textContent = `Agregar`;
+    boton.setAttribute(`id`, `add`);
+    boton.addEventListener(`click`, addCard);
+    formNewCard.appendChild(boton);
+}
+
+
+
+function addCard(){
+    let nombre = document.getElementById(`nombre`).value;
+    let tarjeta = document.getElementById(`tarjeta`).value;
+    let numero = parseInt(document.getElementById(`numero`).value);
+    let ccv = parseInt(document.getElementById(`ccv`).value);
+
+    arrayDeTarjetas.push( new Tarjeta(nombre, tarjeta, numero, ccv) );
+
+    // Agrego una tarjeta a la lista, saco el formulario y muestro las tarjetas
+    formNewCard.innerHTML = ``;
+    mostrarTarjetas();
+}
+
+
+
+function eliminarTarjeta(e){
+    // Accedo al número de la tarjeta a eliminar
+    let num = parseInt(e.currentTarget.parentNode.children[2].innerHTML);
+    let index = arrayDeTarjetas.map(t => t.number).indexOf(num);
+    
+    // Elimino la tarjeta de arrayDeTarjetas
+    arrayDeTarjetas.splice(index,1);
+    mostrarTarjetas();
+}
+
+
+
+//              Funciones normales
+function mostrarTarjetas(){
+    listaDeTarjetas.innerHTML = ``;
+    
+    for (let i = 0; i < arrayDeTarjetas.length; i++) {
+        let li = document.createElement(`li`);
+        li.setAttribute(`id`, `tarjeta${i}`);
+
+        li.innerHTML = `
+            <p class="datos-tarjeta">${arrayDeTarjetas[i].name}</p>
+            <p class="datos-tarjeta">${arrayDeTarjetas[i].tarjeta}</p>
+            <p class="datos-tarjeta">${arrayDeTarjetas[i].number}</p>
+            <button class="quitarTarjeta">x</button>
+        `;
+
+        listaDeTarjetas.appendChild(li);
+        let vector = document.getElementsByClassName(`quitarTarjeta`);
+        vector[i].addEventListener(`click`,eliminarTarjeta);
+
         
-        const sign = this.rec ? "+" : "-";
-        li.innerHTML = `<li>${sign} $${this.amount}\n  Fecha: ${this.fecha}</li>`;
-        nodoLista.appendChild(li);  // Agrego ítem a la lista
     }
+    
 }
 
 
 
 
-// Billetera
-class Wallet{
-    constructor(user){
-        this.usuario     = user;
-        this.saldo       = 0;
-        this.n           = 0;       // Cantidad de transferencias
-        this.arrayTransf = [];      // Array de objetos transferencias
-    }
-    
-    ingresar(valor, fecha){
-        if(valor >= 0){
-            this.saldo += valor;
-            this.arrayTransf.push( new Transferencia(true, valor, fecha) );
-            this.n++;
-
-            alert(`Ingresaste $${valor} a tu cuenta.`);
-        } else
-        alert(`Valor incorrecto!`);
-    }
-    
-    pagar(valor, fecha){
-        if(this.saldo >= valor){
-            this.saldo -= valor;
-            this.arrayTransf.push( new Transferencia(false, valor, fecha) );
-            this.n++;
-
-            alert(`Pagaste $${valor}.`);
-        } 
-        else
-        alert(`Saldo insuficiente.`);
-    }
-    
-    consultarInfo(){
-
-        if (this.n > 0){
-            let nodoSection = document.getElementById("transferencias");
-            nodoSection.innerHTML = `
-                                <h2>${this.n} transferencias</h2>
-                                <ul id="listaTransferencias"></ul>
-                                `;
-        }
 
 
-        for (let i = 0; i < this.n; i++)
-            this.arrayTransf[i].info();
+//              MAIN
 
-        alert(`Usuario: ${this.usuario}\nSaldo actual: $${this.saldo}.`);
-    }
-
-    sortedInfo(ordenarPor){
-        if(this.n > 0){
-            let backup = this.arrayTransf;
-
-            this.arrayTransf.sort((a,b) => a[ordenarPor] - b[ordenarPor]);
-
-            this.consultarInfo();
-            this.arrayTransf = backup;
-        }
-    }
-}
-// Fin Declaración de clases
-
-
-
-
-//----------MAIN----------
-
-// Creo una sola billetera
-const myWallet = new Wallet(prompt("Ingresar usuario de la billetera"));
-let amount, fecha, option;
-
-
-
-// Menú principal
-do {
-    option = parseInt(prompt("Opciones\n\n" +
-                            "(1) Ingresar dinero\n" +
-                            "(2) Pagar\n" +
-                            "(3) Ver movimientos"));
-
-    switch (option) {
-        case 1:
-            amount = parseFloat(prompt("Monto a ingresar"));
-            fecha = parseInt(prompt("Fecha:"));
-            myWallet.ingresar(amount, fecha);
-            break;
-
-        case 2:
-            amount = parseFloat(prompt("Monto a pagar"));
-            fecha = parseInt(prompt("Fecha:"));
-            myWallet.pagar(amount, fecha);
-            break;
-
-        case 3:
-            let option2 = parseInt(prompt(`Ordenar por:\n\n(1) Fecha\n(2) Monto`));
-            switch(option2){
-                case 1: myWallet.sortedInfo("fecha"); break;
-                case 2: myWallet.sortedInfo("amount"); break;
-                default:;
-            }
-            break;
-
-        default:
-            alert("Adios!");
-            break;
-    }
-} while (option > 0 && option < 4);
+const arrayDeTarjetas = [];
+arrayDeTarjetas.push( new Tarjeta(`USUARIO LOCAL`, `visa`, 23422342, 574) );
+mostrarTarjetas();
