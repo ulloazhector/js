f335b602a06022c4ae68900777e79296e391aaa4
Héctor Ulloa <ulloazhector@gmail.com>
2021-11-16 16:40:02 -0300

    Segunda entrega corregida

--- main.js
+++ main.js
@@ -1,117 +1,361 @@
+// Templates
+const pageContent       = document.getElementById(`page-content`);
+const tmpltDashboard    = document.getElementById(`tmplt-dashboard`).content;
+const tmpltSignUp       = document.getElementById(`tmplt-user-signup`).content;
+const tmpltSignIn       = document.getElementById(`tmplt-user-signin`).content;
+const tmpltSidebar      = document.getElementById(`tmplt-sidebar`).content;
+const tmpltMiCuenta     = document.getElementById(`tmplt-mi-cuenta`).content;
+const tmpltMiActividad  = document.getElementById(`tmplt-mi-actividad`).content;
+const tmpltTransferir   = document.getElementById(`tmplt-transferir`).content;
 
-// BOOTSTRAP
+const fragment = document.createDocumentFragment();
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+// Expresiones regulares
+const isValidUsername   = name  =>  /^\w+([\.-]?\w+)*$/.test(name);
+const isValidPin        = pin   =>  /^\d{4}$/.test(pin);
+const timeFormat        =           /\d{2}:\d{2}:\d{2}/;
 
+// Usuario actual
+let lastUser = {};
+
+
+const bgGradiente = `linear-gradient(36deg, rgba(151,167,208,1) 0%, rgba(255,255,255,1) 100%)`
 
 
 
 
+// Al ingresar por primera vez
+pageContent.style.height = `100vh`;
+pageContent.style.background = bgGradiente;
 
 
+// Log in
+function loadSignInForm() {
 
-
-
-  // Eventos Sidebar
-  //////////////////////////////////////////////////////////////////////
-  document.getElementById(`seccion-actividad`).style.display = `none`;
-  document.getElementById(`seccion-perfil`).style.display = `none`;
-  
-  let menuTarjetas = document.getElementById(`menu-tarjetas`);
-  let menuActividad = document.getElementById(`menu-actividad`);
-  let menuPerfil = document.getElementById(`btn-editar-perfil`);
-  
-  menuTarjetas.addEventListener(`click`, selectTarjetas);
-  menuActividad.addEventListener(`click`, selectActividad);
-  menuPerfil.addEventListener(`click`, selectPerfil);
-  
-
-
-
-  // Cambio los displays del documento, para mostrar la seccion seleccionada
-  function selectTarjetas(){
-      menuTarjetas.classList.add(`active`);
-      menuActividad.classList.remove(`active`);
-      document.getElementById(`seccion-tarjetas`).style.display = `block`;
-      document.getElementById(`seccion-actividad`).style.display = `none`;
-      document.getElementById(`seccion-perfil`).style.display = `none`;
-    }
+    pageContent.innerHTML = ``;
+    pageContent.classList.add(`d-flex`, `flex-column`, `justify-content-center`, `align-items-center`);
     
-    function selectActividad(){
-        menuActividad.classList.add(`active`);
-        menuTarjetas.classList.remove(`active`);
-        document.getElementById(`seccion-actividad`).style.display = `block`;
-        document.getElementById(`seccion-tarjetas`).style.display = `none`;
-        document.getElementById(`seccion-perfil`).style.display = `none`;
-    }
+    // Cargo Sign in
+    let clone = tmpltSignIn.cloneNode(true);
+    fragment.appendChild(clone);
+    pageContent.appendChild(fragment);
 
-    function selectPerfil(){
+    const btnSignin             = document.getElementById(`btn-signin`);
+    const inputUsernameSignin   = document.getElementById(`input-username-signin`);
+    const inputPinSignin        = document.getElementById(`input-pin-signin`);
+    const btnCreateAccount      = document.getElementById(`create-account`);
+
+    btnSignin.addEventListener(`click`, () => {
+
+        const usuarios = [];
+        for(let i = 0; i < localStorage.length; i++)
+            usuarios.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+
+        if (usuarios.find(obj => obj.username   === inputUsernameSignin.value) &&
+        usuarios.find(obj => obj.pin        === inputPinSignin.value)){
+            // El usuario actual es el que se acaba de loguear
+            lastUser = usuarios[usuarios.findIndex(user => user.username === inputUsernameSignin.value)];
+            lastUser.current = true;
+            localStorage.setItem(`user_${lastUser.username}`, JSON.stringify(lastUser));
+
+            loadDashboard();
+        }else{
+            // Alerta si hay datos invalidos
+            btnSignin.parentNode.removeChild(btnSignin.parentNode.lastChild);
+            
+            const error = document.createElement(`p`);
+            error.classList.add(`text-danger`);
+            error.textContent = `Datos invalidos`;
+            btnSignin.parentNode.appendChild(error);
+            
+        }
+
+    })
+
+    btnCreateAccount.addEventListener(`click`, e => {
+        e.preventDefault();
+        loadSignUpForm();
+    });
+
+}
+
+// Sign Up
+function loadSignUpForm() {
+    pageContent.innerHTML = ``;
+    pageContent.classList.add(`d-flex`, `flex-column`, `justify-content-center`, `align-items-center`);
+    
+    // Cargo Sign Up
+    let clone = tmpltSignUp.cloneNode(true);
+    fragment.appendChild(clone);
+    pageContent.appendChild(fragment);
+    
+    const btnSignup     = document.getElementById(`btn-signup`);
+    const inputUsername = document.getElementById(`input-username`);
+    const inputPin      = document.getElementById(`input-pin`);
+    const login         = document.getElementById(`login`);
+    
+    // Al apretar el boton verifico si los inputs son correctos
+    btnSignup.addEventListener(`click`, () => {
+        newUser = {
+            username:   inputUsername.value,
+            pin:        inputPin.value,
+            dinero:     1000,
+            actividad:  [],
+            current:    true
+        };
+        
+        if (!isValidUsername(inputUsername.value))
+        inputUsername.classList.add(`border-danger`);
+        if (!isValidPin(inputPin.value))
+        inputPin.classList.add(`border-danger`);
+        
+        // Si esta todo ok guardo el nuevo usuario y muestro el dashboard
+        const usuarios = [];
+        for(let i = 0; i < localStorage.length; i++)
+            usuarios.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+
+        if (isValidUsername(inputUsername.value) && isValidPin(inputPin.value))
+            if(!usuarios.find(obj => obj.username === inputUsername.value)){
+                // El usuario actual es el que se acaba de registrar
+                lastUser = newUser;
+                localStorage.setItem(`user_${newUser.username}`, JSON.stringify(newUser));
+                loadDashboard();
+            }
+            else {
+                btnSignup.parentNode.removeChild(btnSignup.parentNode.lastChild);
+                
+                const error = document.createElement(`p`);
+                error.classList.add(`text-danger`);
+                error.textContent = `Este username ya existe`;
+                btnSignup.parentNode.appendChild(error);
+            }
+
+    });
+    
+    // Al hacer click en un los inputs incorrectos quito el borde rojo
+    inputUsername.addEventListener(`click`, () => {
+        if (inputUsername.focus)
+        inputUsername.classList.remove(`border-danger`);
+    });
+    
+    inputPin.addEventListener(`click`, () => {
+        if (inputPin.focus)
+            inputPin.classList.remove(`border-danger`);
+    });
+
+    login.addEventListener(`click`, e => {
+        e.preventDefault();
+        loadSignInForm();
+    });
+}
+
+// Dashboard
+const loadDashboard = () => {
+    pageContent.innerHTML = ``;
+    pageContent.classList.remove(`d-flex`, `flex-column`, `justify-content-center`, `align-items-center`)
+
+    const clone = tmpltDashboard.cloneNode(true);
+    fragment.appendChild(clone);
+    pageContent.appendChild(fragment);
+
+
+    // Cargo el sidebar
+    loadMenu();
+    // Cargo el conetenido
+    loadMainContent();
+}
+
+// Cargar menu
+function loadMenu(){
+    const mainMenu = document.getElementById(`main-menu`);
+    const clone = tmpltSidebar.cloneNode(true);
+    fragment.appendChild(clone)
+    mainMenu.appendChild(fragment);
+
+    document.getElementById(`username-sidebar`)
+        .textContent = lastUser.username;
+}
+
+// Cargar contenido
+function loadMainContent() {
+    tmpltMiCuenta.getElementById(`user-card`).textContent = `@${lastUser.username}`;
+    tmpltMiCuenta.getElementById(`dinero-card`).textContent = `$${lastUser.dinero}`;
+
+    const mainContent = document.getElementById(`main-content`);
+    mainContent.textContent = ``;
+    let clone = tmpltMiCuenta.cloneNode(true);
+    fragment.appendChild(clone);
+    mainContent.appendChild(fragment);
+    
+    clone = tmpltTransferir.cloneNode(true);
+    fragment.appendChild(clone);
+    mainContent.appendChild(fragment);
+    
+
+    // Eventos Sidebar a MainContent
+    const menuMiCuenta      = document.getElementById(`menu-mi-cuenta`);
+    const menuActividad     = document.getElementById(`menu-actividad`);
+    const btnSalirSidebar   = document.getElementById(`btn-salir-sidebar`);
+    const btnLogoutMain     = document.getElementById(`btn-logout`);
+
+
+
+    // Asigno los eventos
+    menuMiCuenta.addEventListener(`click`, e => {
+        e.preventDefault();
+
+        mainContent.textContent = ``;
+
+        // Colores en el sidebar
+        menuMiCuenta.classList.add(`active`);
         menuActividad.classList.remove(`active`);
-        menuTarjetas.classList.remove(`active`);
-        document.getElementById(`seccion-perfil`).style.display = `block`;
-        document.getElementById(`seccion-actividad`).style.display = `none`;
-        document.getElementById(`seccion-tarjetas`).style.display = `none`;
-    }
+        loadDashboard();
+
+        let clone = tmpltMiCuenta.cloneNode(true);
+        fragment.appendChild(clone);
+        mainContent.appendChild(fragment);
+
+    });
 
 
+    menuActividad.addEventListener(`click`, e => {
+        e.preventDefault();
+
+        mainContent.textContent = ``;
+
+        // Colores en el sidebar
+        menuMiCuenta.classList.remove(`active`);
+        menuActividad.classList.add(`active`);
+
+        // let clone = tmpltMiActividad.cloneNode(true);
+        // fragment.appendChild(clone);
+        // mainContent.appendChild(fragment);
+        loadActivity();
+    });
 
 
-
-    // Eventos Editar perfil
-    //////////////////////////////////////////////////////////////////////
-    let btnGuardar = document.getElementById(`guardar-info`);
-    let userFirstName, userLastName, userEmail;
-
-    btnGuardar.addEventListener(`click`, actualizarInfoUsuario);
-
-    function actualizarInfoUsuario() {
-        userFirstName   = document.getElementById(`usuario-nombre`);
-        userLastName    = document.getElementById(`usuario-apellido`);
-        userEmail       = document.getElementById(`usuario-correo`);
-
-        let usuario = { FirstName:  userFirstName.value,
-                        LastName:   userLastName.value,
-                        Email:      userEmail.value};
-
-        // Actualizo informacion del usuario
-        usuarioJson = JSON.stringify(usuario);
-        localStorage.setItem(`infoUsuario`, usuarioJson);
-
-        // Limpio los inputs
-        userFirstName.value = ``;
-        userLastName.value  = ``;
-        userEmail.value     = ``;
-
-        refreshMenu();
-    }
-
-    let userMenu = document.getElementById(`user-menu`);
-
-    function refreshMenu(){
-        // Si no hay ningun usuario registrado se muestr User en el menu
-        let lastUser = JSON.parse( localStorage.getItem(`infoUsuario`) );
-        userMenu.innerHTML = (localStorage.length) ? lastUser.FirstName : `User`;
-    }
+    btnSalirSidebar.addEventListener(`click`, e => {
+        e.preventDefault();
+        lastUser.current = false;
+        localStorage.setItem(`user_${lastUser.username}`, JSON.stringify(lastUser));
+        loadSignInForm();
+    });
 
 
+    btnLogoutMain.addEventListener(`click`, e => {
+        e.preventDefault();
+        lastUser.current = false;
+        localStorage.setItem(`user_${lastUser.username}`, JSON.stringify(lastUser));
+        loadSignInForm();
+    });
+
+
+    // Eventos modal
+    const inputAmount   = document.getElementById(`input-amount`);
+    const inputDestiny  = document.getElementById(`input-username-destiny`);
+    const btnSend       = document.getElementById(`btn-send`);
+
+    btnSend.addEventListener(`click`, () => {
+
+        const usuarios = [];
+        for(let i = 0; i < localStorage.length; i++)
+            usuarios.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+
+            // Verifico que exista la cuenta destino
+            if ( usuarios.find( user => user.username === inputDestiny.value) 
+            && ( lastUser.username !== inputDestiny.value ) ){
+
+                // Verifico la cantidad
+                if( ( parseInt(inputAmount.value) <= lastUser.dinero)
+                && ( parseInt(inputAmount.value) > 0) ){
+                    // Transfiero
+                    
+                    
+                    const destinyUser = usuarios[usuarios.findIndex(user => user.username === inputDestiny.value)];
+
+                    let fechaTrsf = new Date(Date.now());
+                    let horaTrsf = timeFormat.exec(fechaTrsf.toUTCString());
+                    
+                    
+                    // Actualizo datos origen
+                    lastUser.dinero -= parseInt(inputAmount.value);
+                    lastUser.actividad.push({
+                        env:    1,
+                        amt:    parseInt(inputAmount.value),
+                        orgdes: inputDestiny.value,
+                        hora:   horaTrsf[0]
+                    });
+                    // Sobreescribo el usuario en localStorage
+                    localStorage.setItem(`user_${lastUser.username}`, JSON.stringify(lastUser));
+                    
+                    
+                    // Actualizo datos destino
+                    destinyUser.dinero += parseInt(inputAmount.value);
+                    destinyUser.actividad.push({
+                        env:    0,
+                        amt:    parseInt(inputAmount.value),
+                        orgdes: lastUser.username,
+                        hora:   horaTrsf[0]
+                    });
+                    // Sobreescribo el usuario en localStorage
+                    localStorage.setItem(`user_${destinyUser.username}`, JSON.stringify(destinyUser));
+                    
+                    
+                    loadMainContent();
+                    
+                }
+
+            }
+
+    });
+}
+
+function loadActivity(){
+    const mainContent = document.getElementById(`main-content`);
+    mainContent.textContent = ``;
     
-    refreshMenu();
+    if (lastUser.length != 0){
+        if (lastUser.actividad.length != 0){
+            lastUser.actividad.forEach(actividad => {
+                // Modifico cada card
+                tmpltMiActividad.querySelector(`p`).textContent = actividad.hora;
+                tmpltMiActividad.querySelector(`h3`).textContent =  `${actividad.env ? `-` : `+`} 
+                $${actividad.amt}`;
+                tmpltMiActividad.querySelector(`h5`).textContent = `Cuenta: @${actividad.orgdes}`;
+                tmpltMiActividad.querySelector(`.card`).classList.remove(`border-danger`, `border-success`);
+                tmpltMiActividad.querySelector(`.card`).classList
+                                .add(`${actividad.env ? `border-danger` : `border-success`}`); 
+
+                let clone = tmpltMiActividad.cloneNode(true);
+                fragment.prepend(clone);
+            });
+            mainContent.appendChild(fragment);
+        } else {
+            // No hubo actividades
+            h2 = document.createElement(`h2`);
+            h2.textContent = `No hubo actividades hasta ahora`
+            mainContent.appendChild(h2);
+        }
+    }
+}
+
+
+
+
+
+
+// MAIN
+
+// Chequear si hay cuentas y si hay una cuenta con sesion iniciada
+if(localStorage.length > 0){
+    const usuarios = [];
+        for(let i = 0; i < localStorage.length; i++)
+            usuarios.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+    
+    lastUser = usuarios.find(user => user.current === true);
+
+    if (lastUser != null)   loadDashboard();
+    else                    loadSignInForm();
+} else {
+    loadSignUpForm();
+}
